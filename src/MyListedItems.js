a1d965e0f43242d57e779a32ec2b9cd21e8c384e
aakash <aakashramesh018@gmail.com>
2023-10-28 17:57:14 +0530

    updated script to filter out listed items

---
+++ src/MyListedItems.js
@@ -0,0 +1,27 @@
+export default MyListedItems =  (marketPlace , nft , account , loading , setLoading , listedItems , setListedItems) => {
+
+  const loadListedItems = async () => {
+    const itemCount = await marketPlace.itemCount();
+    let listedItems = [];
+    for(let i=1 ; i<=itemCount ; i++){
+      const item = await marketPlace.items(i);
+      if(item.seller.toLowerCase() == account && item.sold === false){
+        const uri = await nft.tokenUri(item.tokenId);
+        const response = await fetch(uri);
+        const metadata = await response.json();
+        let Item = {
+          price : item.price,
+          itemId : item.itemId,
+          name : metadata.name,
+          description : metadata.description,
+          file : metadata.file
+        }
+
+        listedItems.push(item);
+      }
+    }
+    
+    setLoading(false);
+    setListedItems(listedItems);
+  }
+}
